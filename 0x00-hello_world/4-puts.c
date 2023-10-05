80c29d78d617a6c2ad9890120ef2844ed024f0b0
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-05 14:48:52 -0700

    Added a new file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main: function that prints a string
+ *
+ * Return: success always
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle"\n);
+	return(0);
+}
