6b53a75f40991d02e8994a844888483232709636
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-08 23:01:10 -0700

    added new file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success always
+ *
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a', alph <= 'z', alph++)
+		putchar(alph);
+	for (alph = 'A', alph <= 'Z', alph++)
+		putchar(alph);
+
+	putchar('\n');
+
+
+
+	return (0);
+}
