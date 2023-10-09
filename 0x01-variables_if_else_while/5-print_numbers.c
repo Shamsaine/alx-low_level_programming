693e1851cd268e368d428ea18ad1515bfa3b10dd
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-08 23:57:49 -0700

    added new file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success always
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x);
+
+
+	return (0);
+}
