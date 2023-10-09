8a4bba30ae65dd4edf5332ffb3439085d337f084
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-08 23:38:06 -0700

    added new file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success always
+ *
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'e' && alph != 'q')
+			putchar(alph);
+	
+	}
+	putchar('\n');
+
+
+	return (0);
+}
