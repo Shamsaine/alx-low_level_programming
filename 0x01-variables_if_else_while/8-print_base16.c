395972f97fe4bcc2feea54d340a0bbd7a354a525
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-09 22:01:13 -0700

    added new file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success always
+ */
+
+int main(void)
+{
+	int x;
+	char alph;
+
+	for (x = 0; x < 10; x++)
+		putchar((x % 10) + '0');
+
+	for (alph = 'a'; alph <= 'f'; alph++)
+		putchar(alph);
+`
+	putchar('\n');
+
+	return (0);
+}
