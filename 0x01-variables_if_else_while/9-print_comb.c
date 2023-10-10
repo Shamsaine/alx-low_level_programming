0954000564bc62a2ecf5b30595528e71c22278ce
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-09 22:05:04 -0700

    added new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success always
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar((x % 10) + '0');
+		if (x == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
