e864898aed823f03c294651ac7299752351d2e9b
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-11 07:44:22 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase followed by a new line
+ *
+ * Return: success always
+ *
+ */
+
+int main(void)
+{
+	int x;
+	for (x = a; x <= z; x++;)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+	return (0);
+}
