b5e92f84607f347b717969a56395a9668d70128c
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-11 13:54:30 -0700

    added a new file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,13 +12,11 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char x;
 
-	while (i <= 9)
+	while (i++ <= 10)
 	{
 		for (x = 'a'; x <= 'z'; x++)
 			_putchar(x);
 	}
-
-	i++;
 
 	_putchar('\n');
 }
