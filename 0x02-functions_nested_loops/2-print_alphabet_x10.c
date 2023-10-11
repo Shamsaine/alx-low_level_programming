824ae4932f30fd2ab7353714ca30ad3d506c7621
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-11 09:13:12 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabets in lower case
+ *
+ * Return: success always
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x*10);
+	_putchar('\n')
+}
