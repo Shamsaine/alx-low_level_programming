051f8592cdfcf8ebb18905733363e4f27da4772e
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-11 07:08:11 -0700

    added a new file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,25 @@
-#include <main.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: success always
+ *
+ */
+
+int main()
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar('\n');
+
+
+	return (0);
+
+}
