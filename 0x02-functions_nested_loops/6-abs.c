41d4c04062e584a151af760433479e3484962610
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-11 19:14:17 -0700

    added a new file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,16 +3,16 @@
 /**
  * _abs - function that computes the absolute value of an integer
  *
+ * @n: function argument
+ *
  * Return: success always
  *
  */
 
-int _abs(int)
+int _abs(int n)
 {
-
-
-
-
-
-	return (0);
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 }
