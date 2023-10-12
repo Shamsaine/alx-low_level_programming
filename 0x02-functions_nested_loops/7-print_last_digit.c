711d98b241e1c99105af2974966d5d8ca0f73368
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-11 19:34:20 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *
+ * @n: function argument
+ *
+ * Return: success always
+ *
+ */
+
+int print_last_digit(int)
+{
+
+
+	return (0);
+}
