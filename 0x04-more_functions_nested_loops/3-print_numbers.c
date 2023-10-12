de650e2f929bb7e3248a6cf050886d0574a822d1
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-12 09:20:30 -0700

    Added a new file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9
+ *
+ * Return: success always
+ *
+ */
+
+void print_numbers(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		_putchar(x);
+		_putchar('\n');
+		x++;
+	}
+
+}
