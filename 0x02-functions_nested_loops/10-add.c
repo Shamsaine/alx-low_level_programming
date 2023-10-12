0eebb07a8e3d7d51dd98b0eb28a300d72038084b
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-11 21:52:49 -0700

    added a new file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: success always
+ *
+ */
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+
+}
