b79a5db3205b0a7ab6e433335ebd647c7f7f4632
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-12 17:24:56 -0700

    Added new file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Test function for positive or negative
+ *
+ * Return: success always
+ *
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	
+	positive_or_negative(i);
+	
+	return (0);
+}
+
