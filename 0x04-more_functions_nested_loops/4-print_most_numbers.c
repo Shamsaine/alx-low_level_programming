aaead2693e3c281476a3361de1f21ec3419c2fbf
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-13 13:14:23 -0700

    Added a new file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,11 @@ void print_most_numbers(void)
 
 	while (x <= 9)
 	{
-		if (x == 2 || x == 4)
-			_putchar('\n');
-		else
+		if (x != 2 && x != 4)
 			_putchar(x + '0');
+
+		_putchar('\n');
+		
 		x++;
 	}
 }
