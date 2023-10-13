7f4a7da72d82442daa1ee40e09dfd15516ae797e
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-13 14:40:51 -0700

    Added a new file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,16 @@
 
 void more_numbers(void)
 {
-	int x;
+	int x, y;
 
-	for (x = 0; x <= 14; x++)
+	for (y = 0; y <= 9; y++)
 	{
-		_putchar((x) * 10);
+		for (x = 0; x <= 14; x++)
+		{
+			if (x > 9)
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
