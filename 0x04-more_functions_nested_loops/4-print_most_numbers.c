1634058f21166a44176cfeb554db0ff6c1921883
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-13 12:07:34 -0700

    Added a new file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,14 @@
 
 void print_most_numbers(void)
 {
+	int x = 0;
 
-
-
-
+	while (x <= 9)
+	{
+		if (x == 2 || x == 4)
+			_putchar('\n');
+		else
+			_putchar(x);
+		x++;
+	}
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers from 0 - 14
+ *
+ * Return: success always
+ *
+ */
+
+void more_numbers(void)
+{
+
+
+}
