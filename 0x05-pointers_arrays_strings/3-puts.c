f5f59a35939c914f9133b0f8985c5f2e45dd6569
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-17 07:36:01 -0700

    Added new file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: function argument
+ *
+ * Return: success always
+ *
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	
+	}
+	putchar(10);
+
+}
