8247a1a1334b4a71fc43ae853c15d382a00f0433
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-17 04:41:39 -0700

    Added new file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -13,5 +13,5 @@
 void reset_to_98(int *n)
 {
 	int x = 98;
-	*n = x;	
+	*n = x;
 }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of teo integers
+ *
+ * @a: first function argument
+ * @b: second function argument
+ *
+ * Return: success always
+ *
+ */
+
+void swap_int(int *a, int *b);
+{
+        int x = 98;
+        *n = x;
+}
+
