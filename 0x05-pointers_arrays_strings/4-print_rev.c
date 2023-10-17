b8f1fe49c4b7a274a44dd974a81f3ae46ce6c166
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-17 07:47:56 -0700

    Added new file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: function argument
+ *
+ * Return: success always
+ *
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != 0)
+	{
+	
+	
+	}
+
+
+}
