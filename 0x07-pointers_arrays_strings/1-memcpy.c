f7a9067f3c4108ab5c8506e175f63eb34883686f
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-23 10:02:24 -0700

    Added new file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copie memory area
+ *
+ * @dest: destination
+ * @src: source 
+ * @n: counting integer
+ *
+ * Return: success always
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+
+
+
+
+	return (dest);
+}
