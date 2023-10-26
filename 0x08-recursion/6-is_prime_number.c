a6da03583bfcda6fa61e583647f7c803d429feb9
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-25 22:07:40 -0700

    added new file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if integer is a prime number else return 0
+ *
+ * @n: function argument
+ *
+ * Return: success always
+ */
+
+int is_prime_number(int n)
+{
+	int x;
+
+	if (x = 1 && n/x == n)
+		return (1);
+	if (x
+
+
+
+}
