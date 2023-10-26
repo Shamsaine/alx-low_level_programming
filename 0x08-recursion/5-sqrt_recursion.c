46c750120ab6f21cce2533c8f2f3927490d3515b
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-25 21:56:49 -0700

    added new file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural squareroot of a number
+ *
+ * @n: function argument
+ *
+ * Return: natural squareroot of a number
+ *
+ */
+
+int _sqrt_recursion(int n)
+{
+	int x;
+
+	if (x * x == n)
+		return (n);
+	else if (
+
+
+}
