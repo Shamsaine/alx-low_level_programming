576fe7c936d6a65f10d91a66a9cb01c32153650d
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-29 04:41:54 -0700

    Added a newfile

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count 
+ * @argv: argument vector
+ *
+ * Return: success always
+ *
+ */
+
+int main(int argc, char **argv)
+{
+
+
+	return (0);
+}
