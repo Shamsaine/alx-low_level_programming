9fb99d313ea16dfdbf97cd391ebae472dc01157e
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-29 03:48:24 -0700

    Added a newfile

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: function argument
+ * @argv: function argument
+ *
+ * Return: success always
+ *
+ */
+
+int main(int argc, char **argv)
+{
+
+
+	return (0);
+}
+
+
