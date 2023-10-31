4f64f3102a9e225ff2e9d0828d969d5bbfc6ecff
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-10-31 04:26:41 -0700

    Added a newfile

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: success always
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	int x, y, z;
+
+	z = 0, y = 0;
+
+	if (argc > 1)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			if (isnumber(argv[x]))
+				y += atoi(argv[x]);
+			else
+				z = 1;
+		}
+	}
+	if (z == 0)
+		printf("%i\n", y);
+	else
+		puts("Error");
+	return (z);
+}
