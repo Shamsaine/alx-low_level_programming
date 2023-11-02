64315e7441ac679419a6db970d3429cdfbe6e46f
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-02 14:28:23 -0700

    Added new file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ *
+ * @b: function argument
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+
+	if (x == NULL)
+		exit(98);
+
+	return (x);
+
+
+}
