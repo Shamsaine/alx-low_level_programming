f22bb4eed841ab9529839391d18085c576747f10
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-06 15:33:48 -0800

    Added Newfile

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,23 @@
 #include "dog.h"
 
 /**
+ * init_dog - function that initializes a variable of tpe struct dog
+ *
+ * @@d: first element of the struct
+ * @name:  dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: success always
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,9 @@
 #include <ctype.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 /**
  * struct dog - a structure created to take dog details
