9861cb36e149daaf86308779c21440677f271f2e
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-06 08:28:37 -0800

    Added Newfile

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+ * struct dog - a structure created to take dog details
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: owners name
+ *
+ */
+
 struct dog
 {
 	char *name;
