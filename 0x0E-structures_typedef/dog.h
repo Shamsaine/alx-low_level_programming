5730646053efd2b281e67a3343633cb78893f3bc
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-06 08:32:11 -0800

    Added Newfile

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
  * struct dog - a structure created to take dog details
