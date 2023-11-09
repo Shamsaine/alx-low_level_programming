a5a3b8ed12126456b704f25d974147e0a6236d9c
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-08 16:13:35 -0800

    Added New file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,15 +12,16 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int x;
+
+
 	if (!array || !cmp)
 		return;
-
-	int x;
 
 	for (x = 0; x < size; x++)
 	{
 		if (cmp(array[x]) != 0)
-			return (x);	
+			return (x);
 	}
 
 	return (-1);
