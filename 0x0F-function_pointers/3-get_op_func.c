05610ff19da0502431eeff349020e015ecf17fca
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-08 17:14:58 -0800

    Added New file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,17 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function that selects the correct operation asked by the user
+ *
+ * @s: operator passed as an argument
+ *
+ * Return: pointer to the function that corresponds to
+ * the operator given as a parameter
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+
+
+
+}
