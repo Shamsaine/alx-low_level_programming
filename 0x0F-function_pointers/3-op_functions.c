a48d3156a8e13b886b30ae85e35762c1113aa973
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-08 17:22:36 -0800

    Added New file

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,10 +2,6 @@
 
 /**
  * op_add - returns the sum of a and b
- * op_sub - returns the difference of a and b
- * op_mul - returns the product of a and b
- * op_div - returns the result of division of a by b
- * op_mod - returns the remainder of the division of a by b
  *
  * @a: first intiger
  * @b: second integer
@@ -18,20 +14,56 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
+/**
+ * op_sub - returns the difference of a and b
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - returns the product of a and b
+ *
+ * @a: first intiger
+ * @b: second integer
+ *
+ * Return: result
+ */
 
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - returns the result of division of a by b
+ *
+ * @a: first intiger
+ * @b: second integer
+ *
+ * Return: result
+ */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
+/**
+ * op_mod - returns the remainder of the division of a by b
+ *
+ * @a: first intiger
+ * @b: second integer
+ *
+ * Return: result
+ */
 
 int op_mod(int a, int b)
 {
