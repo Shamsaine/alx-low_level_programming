94c82ce776a8053366f11abdd55c696c9ec62a8a
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-16 13:15:00 -0800

    working on linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ *
+ * @h: node
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+
+
+
+}
