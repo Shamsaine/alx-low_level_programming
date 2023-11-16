a388d2613c350dffb666ad541c724096eb9a40de
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-16 13:41:23 -0800

    working on linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,15 @@ size_t print_list(const list_t *h)
 	list_t *h = malloc(sizeof(list_t));
 
 	if (h->str == NULL)
+		printf("(nil)");
+	
+	list_t *ptr = NULL;
+	ptr = h;
+
+	while (ptr != 0)
 	{
-		str = "(nil)";
-		return (str);
+		printf("%s\n", ptr->str);
+		ptr = ptr->link;	
 	}
 
 
