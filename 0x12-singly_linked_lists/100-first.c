6d9edfedc97fa9d22dc53b5743600b87b40486b6
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-17 09:17:53 -0800

    working on linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * printLines - prints a statement
+ */
+
+void print_statements() 
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
