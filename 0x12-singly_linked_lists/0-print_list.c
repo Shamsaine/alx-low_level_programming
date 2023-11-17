edd8a7632dc0f2604ea37f069844c9e4e926cf80
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-17 06:09:33 -0800

    working on linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,12 +15,12 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %d\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
-		nodes_counted++;
 		h = h->next;
+		nodes_counted++;
 	}
 
 	return (nodes_counted);
