4881e9d8ff335457d9160d716b4acf083efb97c9
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-17 07:50:37 -0800

    working on linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 
 /**
  * list_len - returns the numbers of element in a linked list
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to the beginning of a list
+ *
+ * @head: first node
+ * @str: string to be added
+ *
+ * Return: the address of new element or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	
+}
