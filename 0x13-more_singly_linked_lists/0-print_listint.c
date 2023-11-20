3ffa86ff72f67fcb218df90c1f66b0939665fc7e
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-20 03:32:10 -0800

    Added new file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements in a linked linkedlist
+ *
+ * @h: head
+ *
+ * Return: success always
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	
+
+
+
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - return the number of element in a linked list
+ *
+ * @h: head
+ *
+ * Return: success always
+ */
+
+size_t listint_len(const listint_t *h)
+{
+
+
+
+
+}
