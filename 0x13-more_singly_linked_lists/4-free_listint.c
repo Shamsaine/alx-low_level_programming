f9698426ea2eaaa645307dda2166d0d95aca33c4
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-21 12:05:33 -0800

    Added new file

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ *
+ * @head: pointer to the first node
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *prev_node = *head;
+	listint_t *next_node;
+
+	while (prev_node != NULL)
+	{
+		next_node = prev_node->next;
+		free(prev_node);
+		prev_node = next_node;
+	}
+}
