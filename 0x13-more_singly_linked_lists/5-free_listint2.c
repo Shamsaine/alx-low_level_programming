a0ef16a9a91a8ee14e94cbd1a39e85628bacf0a0
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-21 19:12:56 -0800

    Added new file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,13 +12,16 @@ void free_listint2(listint_t **head)
 	listint_t *prev_node;
 	listint_t *next_node;
 
-	*prev_node = head;
-
-	while (prev_node != NULL)
+	if (head)
 	{
-		next_node = prev_node->next;
-		free(prev_node);
-		prev_node = next_node;
-	}
+		*prev_node = *head;
+		
+		while (prev_node != NULL)
+		{
+			next_node = prev_node->next;
+			free(prev_node);
+			prev_node = next_node;
+		}
+
 	*head = NULL;
 }
