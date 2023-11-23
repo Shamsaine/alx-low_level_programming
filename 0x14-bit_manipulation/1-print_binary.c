78c9e710234a3477db8f2e7c01cd8c7da68ca70c
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-23 13:12:58 -0800

    added newfiles

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ *
+ * @n: the number
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	
+	while (mask > 0)
+	{
+		if (n & mask)
+			putchar('1');
+		else
+			putchar('0');
+	}
+	mask >>= 1;
+}
