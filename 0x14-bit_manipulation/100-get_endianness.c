25a8b950e2d9e6d179e95487a4ef03ad78d9d7e0
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-24 06:14:26 -0800

    added newfiles

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,10 +6,10 @@
  * Return: 0 if big endian, 1 if little endian
  */
 
-int get_endianness(void) 
+int get_endianness(void)
 {
 	unsigned int num = 1;
 	char *byte = (char *)&num;
 
-	return (int)*byte;
+	return ((int)*byte);
 }
