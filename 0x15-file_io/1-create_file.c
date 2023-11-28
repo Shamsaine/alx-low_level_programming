41815b2827a3b3b43e80ab423a5ee19e84f63cfe
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-11-27 20:22:54 -0800

    Added New file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ *
+ * @filename: name of the file to create
+ * @text_content: the null terminated string to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ * if filename is NULL return -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+		return (-1);
+
+	int descriptor = open(filename, O_WRONLY |
+		       O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		ssize_t bytes_written = write(descriptor,
+				text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(descriptor);
+			return (-1);
+		}
+	}
+
+	close(descriptor);
+
+	return (1);
+}
