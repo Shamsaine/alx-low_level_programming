ae6d6343867a3e4744ff9837920fcdc77be1232e
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-12-01 20:53:13 -0800

    Added New file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,27 +11,30 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int descriptor;
+	ssize_t bytes_written;
+
 	if (filename == NULL)
 		return (-1);
 
-	int file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	descriptor = open(filename, O_WRONLY | O_APPEND);
 
-	if (file_descriptor == -1)
+	if (descriptor == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		ssize_t bytes_written = write(file_descriptor, text_content,
+		bytes_written = write(descriptor, text_content,
 				strlen(text_content));
-		
+
 		if (bytes_written == -1)
 		{
-			close(file_descriptor);
+			close(descriptor);
 			return (1);
 		}
 	}
 
-	close(file_descriptor);
-	return (i);
+	close(descriptor);
+	return (1);
 }
 
